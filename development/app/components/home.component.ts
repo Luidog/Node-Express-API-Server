9b5376b5460b6cf27559c36ebaf938136d973dd6
Lui de la Parra <Luidelaparra@mac.com>
2016-03-14 00:11:25 -0700

    Keynote update
    Signed-off-by: Lui de la Parra <Luidelaparra@mac.com>

--- development/app/components/home.component.ts
+++ development/app/components/home.component.ts
@@ -4,7 +4,7 @@ import { Component } from 'angular2/core'
 	selector: 'home-page',
 	template: `
 		<div>
-			Hello - world!!!
+			<img style="padding-left:35%; padding-top:5%;"width="737" height="556" alt="ASCII-apple logo" src="http://api.ning.com/files/OTRhyu1*Ip38MTLctq-b*SBmfLipjdsfOFZ6dd2h8tQ_/ASCIIapple_logo.gif?width=737&amp;height=556">
 		</div>
 	`
 })
